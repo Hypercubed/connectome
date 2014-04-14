e198aa703d54d552750cd751c6d12c75f7f4a314
J. Harshbarger <hypercubed@gmail.com>
2014-04-14 19:13:32 +0900

    WIP

--- app/scripts/components/slider/slider-directive.js
+++ app/scripts/components/slider/slider-directive.js
@@ -12,20 +12,56 @@
       return {
         scope: {
           value: '=ngModel',
-          max: '='
+          max: '=',
+          min: '='
         },
         templateUrl: baseUrl+'slider.html',
         link: function(scope, element, attrs) {
           element.addClass('slider');
 
-          scope.$watchCollection('[slider,max,value]', function() {
-            scope.value = scope.value || 0;
-            scope.max = scope.max || 100;
 
-            scope.percent = scope.value/scope.max*100;
+          scope.internalValue = scope.value;
+          scope.percent = (scope.value-scope.min)/(scope.max-scope.min)*100;
+          
+          scope.$watch('value', changeInputValue);
+          scope.$watch('max', change);
+          scope.$watch('min', change);
+          scope.$watch('percent', changeInternalValue);
+
+          function changeInputValue(newVal, oldVal) {
+            if (newVal == oldVal) return;
+
+            //scope.value = +scope.value || 0;
+            scope.internalValue = newVal;
+            scope.percent = (newVal-scope.min)/(scope.max-scope.min)*100;
+            change();
+          }
+
+          var stop;
+          function changeInternalValue(newVal, oldVal) {
+            if (newVal == oldVal) return;
+
+            //scope.percent = +scope.percent || 0;
+            scope.internalValue = scope.percent/100*(scope.max-scope.min)+scope.min;
+            change();
+
+            $timeout.cancel(stop);
+            stop = $timeout(function() {
+              scope.value = scope.internalValue;
+            }, 10);
+            
+          }
+
+          function change() {
+            
+            scope.max = +scope.max || 100;
+            scope.min = +scope.min || 0;
+
             scope.left = scope.percent > 10;
-            scope.right = scope.percent < 90;
-          });
+            scope.right = scope.percent < 90;    
+            scope.bubbleStyle = {left: scope.percent+'%'};
+
+          }
 
         }
       };
--- todo.md
+++ todo.md
@@ -2,11 +2,12 @@
 
 _\( managed using [todo-md](https://github.com/Hypercubed/todo-md) \)_
 
-- [ ] localStorage
-- [ ] filters
-- [ ] Use select2
+- [x] localStorage
+- [x] filters
+- [x] Use select2
 - [ ] Add pull out menu
-- [ ] range directive?
-- [ ] Fix loading of range.min
-- [ ] Limit number of selected cels
+- [x] range directive?
+- [x] Fix loading of range.min
 - [ ] use vector lib?  (https://github.com/llafuente/js-2dmath)
+- [x] Debounce slider
+- [ ] Add log scale to sliders
