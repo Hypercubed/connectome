4b19dc0dcd403e390c19c4ab4a704044173b196b
J. Harshbarger <hypercubed@gmail.com>
2014-04-30 13:55:38 +0900

    Improved sliders

--- app/components/slider/slider-directive.js
+++ app/components/slider/slider-directive.js
@@ -17,41 +17,63 @@
           step: '=?'
         },
         templateUrl: baseUrl+'slider.html',
+        compile: function(tElement, tAttrs, transclude) {
+          return this.link
+        },
         link: function(scope, element) {
           element.addClass('slider');
+          var $range = element.find('input[type=range]');
 
-          scope.max = +scope.max || 10000;
+          scope.max = +scope.max || 1;
           scope.min = +scope.min || 0;
-          scope.step = +scope.step || 1;
+          scope.step = +scope.step || 0.01;
+          scope.fractionSize = parseInt(Math.log(scope.step)/Math.log(0.1));
+          console.log(scope.fractionSize);
           scope.value = +scope.value || 0;
+          scope.rangeValue = +scope.value || 0;
+          scope.numberValue = +scope.value || 0;
 
-          var applyValue = debounce(function() {
-            var val = scope.rangeValue*(scope.max-scope.min);
-            scope.value = Math.round(val)/100+scope.min;
+          var applyValue = debounce(function(value) {
+            scope.value = +value;
           }, 100);
 
           function changeRangeValue(newVal, oldVal) {
-            if (+newVal === +oldVal) {return;}
 
             scope.rangeValue = +newVal;
             updateDom();
 
-            applyValue();
+            applyValue(+newVal);
+          }
+
+          function changeNumberValue(newVal, oldVal) {
+
+            scope.numberValue = +newVal;
+            updateDom();
+
+            applyValue(+newVal);
           }
 
           function updateRange() {
-            scope.rangeValue = (scope.value-scope.min)/(scope.max-scope.min)*100;
+
+            $range.attr('max', scope.max);  // Need to update range before value.
+            $range.attr('min', scope.min);
+            $range.attr('step', scope.step);
+
+            scope.rangeValue = +scope.value;
+            scope.numberValue = +scope.value;
             updateDom();
           }
 
           function updateDom() {
-            scope.left = scope.rangeValue > 10;
-            scope.right = scope.rangeValue < 90;
+            scope.percent = (+scope.value-scope.min)/(scope.max-scope.min)*100;
+            scope.left = scope.percent > 10;
+            scope.right = scope.percent < 90;
           }
 
           updateRange();
           
           scope.$watch('rangeValue', changeRangeValue);
+          scope.$watch('numberValue', changeNumberValue);
           scope.$watchCollection('[value,max,min,step]', updateRange);
 
         }
